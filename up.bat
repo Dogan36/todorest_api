c9df8733953184b201e70ba72c3ff12526628890
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2024-08-02 17:33:32 +0200

    $*

--- up.bat
+++ up.bat
@@ -4,4 +4,4 @@ git commit -m "$*"
 
 git push
 
-chrome "https://www.pythonanywhere.com/user/Dogan36/consoles/34988645/"
+start chrome "https://www.pythonanywhere.com/user/Dogan36/consoles/34988645/"
