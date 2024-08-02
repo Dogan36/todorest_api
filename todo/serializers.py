0a3a4dcce30d42912e7ca205d1c082380033bb86
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2024-08-02 18:01:00 +0200

    $*

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,9 +1,20 @@
+from dataclasses import fields
+import email
+from email.policy import default
+from pyexpat import model
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Todo
 
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'username', 'email']
+        
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only = True, default=serializers.CurrentUserDefault())
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'created_at']
+        fields = ['id', 'title', 'description', 'created_at', 'user']
+        
